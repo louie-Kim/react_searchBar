4bad13469106f8fc6d7d533df2f93f42eabb65d2
JY <sewhang7@gmail.com>
2024-11-04 15:20:12 +0900

    !@@!

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchPosts, selectFilteredPosts, setPage, setPageAndFetchPosts, setPageGroup } from './redux/PostSlice';
 import { useInView } from "react-intersection-observer"
